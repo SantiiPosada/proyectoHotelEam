6763aae8541e21a59a6ddcf253468ce78fde42f6
santiago <santiago@SantiagosPc>
2020-05-13 20:55:50 -0500

    cosas varias

--- src/Dao/DaoHuesped.java
+++ src/Dao/DaoHuesped.java
@@ -62,7 +62,7 @@ public class DaoHuesped {
         return desicion;
     }
 
-    public Huesped buscarEstudiante(String cedula) {
+    public Huesped buscarEstudiante(String cedula) throws DatosIncompletosException {
         Huesped huesped = new Huesped();
         try (Connection con = Conexion.getConnection()) {
             PreparedStatement pstmt = con.prepareStatement("SELECT  id,cedula,nombreCompleto,genero,correo,telefono,fechaNacimiento,nacionalidad,contrasena,tipo,estado FROM huesped where cedula=?");
@@ -86,28 +86,55 @@ public class DaoHuesped {
             }
         } catch (SQLException ex) {
             huesped = null;
-            ex.printStackTrace();
+            throw new DatosIncompletosException();
 
         }
         return null;
     }
 
-    /**
-     * metodo extraer la variable que tuvo el codigo de error 1062
-     *
-     * @param variable mensaje de error de sql (ex.getMessage())
-     * @return nombre de la veriable que tiene el error
-     */
-    private String extraerVariable(String variable) {
-        int inicio = variable.indexOf(".");
-        int fin = variable.indexOf("'", inicio + 1);
-        return variable.substring(inicio + 1, fin);
-    }
+    public ArrayList<Huesped> listarEstudiante() {
+        try (Connection con = Conexion.getConnection()) {
 
-//
-//    public boolean modificarEstudiante(Estudiante estudiante) {
+            PreparedStatement pstmt = con.prepareStatement("SELECT  id,cedula,nombreCompleto,genero,correo,telefono,fechaNacimiento,nacionalidad,contrasena,tipo,estado FROM huesped");
+
+            ResultSet respuesta = pstmt.executeQuery();//Me va a traer todo lo que venga como resultado
+            ArrayList<Huesped> listar = new ArrayList<>();
+
+            boolean condicion = true;
+            while (condicion == true) {
+                if (respuesta.next()) {//si respuesta.next(revisa si hay un elemtento,salta al siguiente reistro) devuelve true=si encontro algo o false si no lo encontró
+                    Huesped huesped = new Huesped();
+
+                    huesped.setId(respuesta.getInt("id"));
+                    huesped.setCedula(respuesta.getString("cedula"));
+                    huesped.setNombrecompleto(respuesta.getString("nombreCompleto"));
+                    huesped.setGenero(respuesta.getString("genero"));
+                    huesped.setCorreo(respuesta.getString("correo"));
+                    huesped.setTelefono(respuesta.getString("telefono"));
+                    huesped.setFechanacimiento(respuesta.getDate("fechaNacimiento"));
+                    huesped.setNacionalidad(respuesta.getString("nacionalidad"));
+                    huesped.setContrasena(respuesta.getString("contrasena"));
+                    huesped.setTipo(respuesta.getString("tipo"));
+                    huesped.setEstado(respuesta.getString("estado"));
+
+                    listar.add(huesped);
+
+                } else {
+                    condicion = false;
+                }
+            }
+
+            return listar;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println("Hubo un error al listar");
+        }
+        return null;
+    }
+    
+//        public boolean modificarEstudiante(Huesped huesped) {
 //        boolean desicion = false;
-//        try (Connection con = conexion.getConnetion()) {
+//        try (Connection con = Conexion.getConnection()) {
 //            PreparedStatement pstmt = con.prepareStatement("UPDATE Estudiante SET  idEstudiante=?, Cedula=?, Nombre=?, Apellido=?, Direccion=?, Telefono=?, Correo=?, Fecha_nacimiento=? WHERE idEstudiante=?");//preparar la sentencia sql(modificar,agregar,eliminar,etc) se llena de izquierda a derecha de 1 en 1(1,2,3)
 //
 //            pstmt.setInt(1, estudiante.getId());
@@ -136,44 +163,25 @@ public class DaoHuesped {
 //
 //        return desicion;
 //    }
-//
-//    public ArrayList<Estudiante> listarEstudiante() {
-//        try (Connection con = conexion.getConnetion()) {
-//
-//            PreparedStatement pstmt = con.prepareStatement("SELECT idEstudiante,Cedula,Nombre,Apellido,Direccion,Telefono,Correo,Fecha_nacimiento FROM Estudiante");
-//
-//            // pstmt.setInt(1, id);
-//            ResultSet respuesta = pstmt.executeQuery();//Me va a traer todo lo que venga como resultado
-//            ArrayList<Estudiante> listaEstudiante = new ArrayList<>();
-//
-//            boolean condicion = true;
-//            while (condicion == true) {
-//                if (respuesta.next()) {//si respuesta.next(revisa si hay un elemtento,salta al siguiente reistro) devuelve true=si encontro algo o false si no lo encontró
-//                    Estudiante estudiante = new Estudiante();
-//
-//                    estudiante.setId(respuesta.getInt("idEstudiante"));
-//                    estudiante.setCedula(respuesta.getString("Cedula"));
-//                    estudiante.setNombre(respuesta.getString("Nombre"));
-//                    estudiante.setApellido(respuesta.getString("Apellido"));
-//                    estudiante.setDireccion(respuesta.getString("Direccion"));
-//                    estudiante.setTelefono(respuesta.getString("Telefono"));
-//                    estudiante.setCorreo(respuesta.getString("Correo"));
-//                    estudiante.setFecha_nacimiento(respuesta.getDate("Fecha_nacimiento"));
-//
-//                    listaEstudiante.add(estudiante);
-//
-//                } else {
-//                    condicion = false;
-//                }
-//            }
-//
-//            return listaEstudiante;
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            System.err.println("Hubo un error al buscar");
-//        }
-//        return null;
-//    }
+    
+    
+    
+
+    /**
+     * metodo extraer la variable que tuvo el codigo de error 1062
+     *
+     * @param variable mensaje de error de sql (ex.getMessage())
+     * @return nombre de la veriable que tiene el error
+     */
+    private String extraerVariable(String variable) {
+        int inicio = variable.indexOf(".");
+        int fin = variable.indexOf("'", inicio + 1);
+        return variable.substring(inicio + 1, fin);
+    }
+
+
+
+
     /**
      * metodo que permite pasar la fecha de tipo java.util.Date a java.sql.Date
      *
